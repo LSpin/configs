f3da8c5746170c7fdfdb0132850ec6069d1c8ca5
LSpin <leonardospinola22@gmail.com>
2026-04-12 16:04:28 -0300

    Add sync script

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+cp ~/.zshrc ./.zshrc
+cp -r ~/.config/nvim/ ./nvim/
+
+git add .
+git diff --cached --quiet && echo "Nothing to sync." && exit 0
+git commit -m "sync: $(date '+%Y-%m-%d %H:%M')"
+git push
