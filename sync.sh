23f80a6e9fbd9ec17dade89f740cee84ad787076
LSpin <leonardospinola22@gmail.com>
2026-04-14 04:31:05 -0300

    sync: 2026-04-14 04:31

--- sync.sh
+++ sync.sh
@@ -1,13 +1,18 @@
 #!/bin/zsh
 
+cd "$(dirname "$0")"
+
 cp ~/.zshrc ./.zshrc
 rsync -a --exclude='.git' ~/.config/nvim/ ./nvim/
 
 INTELLIJ="$HOME/Library/Application Support/JetBrains/IntelliJIdea2025.3"
-cp "$INTELLIJ/options/editor-font.xml"  ./intellij/options/
-cp "$INTELLIJ/options/console-font.xml" ./intellij/options/
-cp "$INTELLIJ/options/ide.general.xml"  ./intellij/options/
-cp "$INTELLIJ/keymaps/macOS copy.xml"   ./intellij/keymaps/
+if [[ -d "$INTELLIJ" ]]; then
+  mkdir -p ./intellij/options ./intellij/keymaps
+  cp "$INTELLIJ/options/editor-font.xml"  ./intellij/options/ 2>/dev/null
+  cp "$INTELLIJ/options/console-font.xml" ./intellij/options/ 2>/dev/null
+  cp "$INTELLIJ/options/ide.general.xml"  ./intellij/options/ 2>/dev/null
+  cp "$INTELLIJ/keymaps/macOS copy.xml"   ./intellij/keymaps/ 2>/dev/null
+fi
 
 git add .
 git diff --cached --quiet && echo "Nothing to sync." && exit 0
